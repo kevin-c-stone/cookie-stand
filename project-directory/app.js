560ba670a504809120d7899485fdb91ae4b046cf
Kevin C Stone <kevin.c.stone@outlook.com>
2021-09-21 10:01:44 -0700

    refactored some of app.js

--- project-directory/app.js
+++ project-directory/app.js
@@ -1,44 +1,80 @@
-hours = ['6am', '7am', '8am', '9am', '10am', '11am', '12am', '1pm', '2pm', '3pm', '4pm', '5pm', '6pm', '7pm'];
+let hours = ['6am', '7am', '8am', '9am', '10am', '11am', '12am', '1pm', '2pm', '3pm', '4pm', '5pm', '6pm', '7pm'];
 
-const storeLocation = [
-  {
-    location: 'Seattle',
-    minCustomer: 23,
-    maxCustomer: 65,
-    avgCookieSales: 6.3,
-    totalCookies: null
-  },
-  {
-    location: 'Tokyo',
-    minCustomer: 3,
-    maxCustomer: 24,
-    avgCookieSales: 1.2,
-    totalCookies: null
-  },
-  {
-    location: 'Dubai',
-    minCustomer: 11,
-    maxCustomer: 38,
-    avgCookieSales: 63.7,
-    totalCookies: null
-  },
-  {
-    location: 'Paris',
-    minCustomer: 20,
-    maxCustomer: 38,
-    avgCookieSales: 2.3,
-    totalCookies: null
-  },
-  {
-    location: 'Lima',
-    minCustomer: 2,
-    maxCustomer: 16,
-    avgCookieSales: 4.6,
-    totalCookies: null
-  }
-];
 
-const randomCustomerGen = function (maxCustomer, minCustomer) {
-  return Math.floor(Math.random() * (maxCustomer - minCustomer + 1) + minCustomer);
+function generateRange(min, max) {
+ let number = Math.floor(Math.random() * (max - min + 1) + min);
+ return number;
+}
+
+
+let seattle = {
+  minCustPerHour: 23,
+  maxCustPerHour: 65,
+  avgCookPerCust: 6.3,
+  cookiesPerHour: [],
+  hourlyCustomers: function(min, max) {
+    for (let i = 0; i < hours.length; i++) {
+     let generateNumber = generateRange(this.minCustPerHour, this.maxCustPerHour);
+
+     this.cookiesPerHour.push(Math.round(generateNumber * this.avgCookPerCust));
+    }
+  },
 };
 
+console.log(seattle);
+seattle.hourlyCustomers();
+
+let tokyo = {
+  minCustPerHour: 3,
+  maxCustPerHour: 24,
+  avgCookPerCust: 1.2,
+  hourlyCustomers: function(min, max) {
+    for (let i = 0; i < hours.length; i++) {
+     let generateNumber = generateRange(this.minCustPerHour, this.maxCustPerHour);
+
+     console.log(Math.round(generateNumber * this.avgCookPerCust));
+    }
+  },
+};
+
+
+let dubai = {
+  minCustPerHour: 11,
+  maxCustPerHour: 38,
+  avgCookPerCust: 63.7,
+  hourlyCustomers: function(min, max) {
+    for (let i = 0; i < hours.length; i++) {
+     let generateNumber = generateRange(this.minCustPerHour, this.maxCustPerHour);
+
+     console.log(Math.round(generateNumber * this.avgCookPerCust));
+    }
+  },
+};
+
+
+let paris = {
+  minCustPerHour: 20,
+  maxCustPerHour: 38,
+  avgCookPerCust: 2.3,
+  hourlyCustomers: function(min, max) {
+    for (let i = 0; i < hours.length; i++) {
+     let generateNumber = generateRange(this.minCustPerHour, this.maxCustPerHour);
+
+     console.log(Math.round(generateNumber * this.avgCookPerCust));
+    }
+  },
+};
+
+
+let lima = {
+  minCustPerHour: 2,
+  maxCustPerHour: 16,
+  avgCookPerCust: 4.6,
+  hourlyCustomers: function(min, max) {
+    for (let i = 0; i < hours.length; i++) {
+     let generateNumber = generateRange(this.minCustPerHour, this.maxCustPerHour);
+
+     console.log(Math.round(generateNumber * this.avgCookPerCust));
+    }
+  },
+};
